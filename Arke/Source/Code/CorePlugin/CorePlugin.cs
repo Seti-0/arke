2db25425d3753fb47d382654de45f2a7e820b83a
Kieran <kcosson111@gmail.com>
2020-05-21 18:55:31 +0100

    General Setup
    Set a suitable namespace according to Duality conventions. Give the
    directory, solution and plugin .dll a friendly name.
    
    Some plugins used by this project aren't covered by the Duality package
    manager, add them to the plugins directory manually. Project references
    need updating accordingly, and exceptions must be added for them
    in the .gitignore.
    
    Delete the starting component.

---
+++ Arke/Source/Code/CorePlugin/CorePlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace Soulstone.Duality.Plugins.Arke
+{
+	public class ArkePlugin : CorePlugin {}
+}
