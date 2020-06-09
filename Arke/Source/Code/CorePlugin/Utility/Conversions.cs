dadb1373729f8ca25552f9e653f9699aac0ce401
Seti-0 <kcosson111@gmail.com>
2020-06-09 16:35:54 +0100

    Add null checks

--- Arke/Source/Code/CorePlugin/Utility/Conversions.cs
+++ Arke/Source/Code/CorePlugin/Utility/Conversions.cs
@@ -10,25 +10,37 @@ using Soulstone.Duality.Plugins.Arke.Backend;
 
 namespace Soulstone.Duality.Plugins.Arke
 {
-    public static class Conversions
+    internal static class Conversions
     {
         public static Backend.IPEndPoint ToArke(System.Net.IPEndPoint ipEnd)
         {
+            if (ipEnd == null) 
+                throw new ArgumentNullException(nameof(ipEnd));
+
             return new Backend.IPEndPoint(ToArke(ipEnd.Address), (ushort)ipEnd.Port);
         }
 
         public static Backend.IPAddress ToArke(System.Net.IPAddress ip)
         {
+            if (ip == null) 
+                throw new ArgumentNullException(nameof(ip));
+
             return new Backend.IPAddress(ip.GetAddressBytes());
         }
 
         public static System.Net.IPEndPoint ToNet(Backend.IPEndPoint ipEnd)
         {
+            if (ipEnd == null) 
+                throw new ArgumentNullException(nameof(ipEnd));
+
             return new System.Net.IPEndPoint(ToNet(ipEnd.Address), ipEnd.Port);
         }
 
         public static System.Net.IPAddress ToNet(Backend.IPAddress ip)
         {
+            if (ip == null) 
+                throw new ArgumentNullException(nameof(ip));
+
             return new System.Net.IPAddress(ip.Bytes);
         }
 
